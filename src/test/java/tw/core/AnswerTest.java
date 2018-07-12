dd6174c51c4ee93aa23e7fa4eec050319c69f5c3
XUHO3 <howells.xu@oocl.com>
2018-07-12 15:15:10 +0800

    write answerTest test,the input is 1234, should return 4A0B

--- src/test/java/tw/core/AnswerTest.java
+++ src/test/java/tw/core/AnswerTest.java
@@ -1,7 +1,14 @@
 package tw.core;
 
 
+import com.google.inject.Injector;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import tw.GuessNumberModule;
+import tw.commands.GuessInputCommand;
+import tw.controllers.GameController;
+
+import static com.google.inject.Guice.createInjector;
 
 /**
  * Created by jxzhong on 2017/9/23.
@@ -12,6 +19,13 @@ public class AnswerTest {
     @BeforeEach
     public void setUp() {
         actualAnswer = Answer.createAnswer("1 2 3 4");
+    }
+
+    @Test
+    public void should_retrun_4A0B_when_input_1_2_3_4()throws Exception{
+       Answer answer=Answer.createAnswer("1 2 3 4");
+       String result=answer.check(answer).getValue();
+       assert result.equals("4A0B");
     }
 
 }
