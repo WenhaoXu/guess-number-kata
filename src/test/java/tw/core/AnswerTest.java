fc171608334b20ca9f90202751f4efcad40e3ab5
XUHO3 <howells.xu@oocl.com>
2018-07-12 15:29:31 +0800

    write answerTest test,the input is 1567, should return 1A0B

--- src/test/java/tw/core/AnswerTest.java
+++ src/test/java/tw/core/AnswerTest.java
@@ -1,14 +1,11 @@
 package tw.core;
 
 
-import com.google.inject.Injector;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import tw.GuessNumberModule;
-import tw.commands.GuessInputCommand;
-import tw.controllers.GameController;
 
-import static com.google.inject.Guice.createInjector;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
 
 /**
  * Created by jxzhong on 2017/9/23.
@@ -24,8 +21,16 @@ public class AnswerTest {
     @Test
     public void should_retrun_4A0B_when_input_1_2_3_4()throws Exception{
        Answer answer=Answer.createAnswer("1 2 3 4");
-       String result=answer.check(answer).getValue();
+       String result=actualAnswer.check(answer).getValue();
        assert result.equals("4A0B");
+    }
+
+    @Test
+    public void should_retrun_1A0B_when_input_1_5_6_7()throws Exception{
+        Answer answer=Answer.createAnswer("1 5 6 7");
+        String result=actualAnswer.check(answer).getValue();
+//        assert result.equals("1A0B");
+        assertThat(result, is("1A0B"));
     }
 
 }
